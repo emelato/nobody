d1c7ed71457362cbedf01cdd84073e0f31b1de6a
emelato <cheolmin012@naver.com>
2023-09-11 17:14:03 +0900

    20230911-3

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// import { Tab } from "./component/20230911/Tab";
+import { Session } from "./component/20230911/Session";
+import { List } from "./component/20230911/List";
+export default function App() {
+  return (
+    <>
+      <Session />
+    </>
+  );
+}
